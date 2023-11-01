78a1b835637aca6c07587527c2ad9f963b6e8f0c
haru1367 <jsk03023@naver.com>
2023-11-01 16:58:12 +0900

    commit

---
+++ .web/pages/index.js
@@ -0,0 +1,80 @@
+import {useEffect, useRef, useState} from "react"
+import {useRouter} from "next/router"
+import {E, connect, updateState} from "/utils/state"
+import "focus-visible/dist/focus-visible"
+import {Button, Container, HStack, Image, Input, Text, VStack, useColorMode} from "@chakra-ui/react"
+import {EmailIcon, LockIcon} from "@chakra-ui/icons"
+import NextHead from "next/head"
+
+const EVENT = "ws://localhost:8000/event"
+export default function Component() {
+const [default_state, setDefault_state] = useState({"events": [{"name": "default_state.hydrate"}]})
+const [result, setResult] = useState({"state": null, "events": [], "processing": false})
+const router = useRouter()
+const socket = useRef(null)
+const { isReady } = router;
+const { colorMode, toggleColorMode } = useColorMode()
+const Event = events => setDefault_state({
+  ...default_state,
+  events: [...default_state.events, ...events],
+})
+useEffect(() => {
+  if(!isReady) {
+    return;
+  }
+  const reconnectSocket = () => {
+    socket.current.reconnect()
+  }
+  if (typeof socket.current !== 'undefined') {
+    if (!socket.current) {
+      window.addEventListener('focus', reconnectSocket)
+      connect(socket, default_state, setDefault_state, result, setResult, router, EVENT)
+    }
+  }
+  const update = async () => {
+    if (result.state != null) {
+      setDefault_state({
+        ...result.state,
+        events: [...default_state.events, ...result.events],
+      })
+      setResult({
+        state: null,
+        events: [],
+        processing: false,
+      })
+    }
+    await updateState(default_state, setDefault_state, result, setResult, router, socket.current)
+  }
+  update()
+})
+return (
+<Container centerContent={true}
+sx={{"justifyContent": "center", "maxWidth": "auto", "height": "100vh", "bg": "#000000"}}><Container centerContent={true}
+sx={{"width": "500px", "height": "75vh", "bg": "#ffffff", "borderRadius": "15px", "boxShadow": "-11px 11px 50px #9ecadb"}}><VStack><Container sx={{"height": "75px"}}/>
+<Container><Text sx={{"fontSize": "25px", "fontWeight": "bolder", "letterSpacing": "5px", "fontFamily": "Georgia, Serif", "background": "-webkit-linear-gradient(-45deg, #fa0000, #f0b46c)", "-webkit-background-clip": "text", "color": "transparent", "centerContent": true}}>{`ONESTARGRAM`}</Text></Container></VStack>
+<VStack><Container sx={{"height": "10px"}}/>
+<Container><Text sx={{"fontSize": "15px", "letterSpacing": "2px", "color": "black", "centerContent": true}}>{`Please log in to access ONESTARGRAM.`}</Text></Container>
+<Container centerContent={true}><Image src="onestar2.ico"
+sx={{"width": "100px", "height": "120px", "alt": "\ub178\ub780\ubcc4"}}/></Container>
+<Container sx={{"borderBottom": "0.3px solid green", "width": "300px", "height": "43px"}}><HStack><EmailIcon sx={{"color": "green", "fontSize": "12px"}}/>
+<Input placeholder="Email"
+focusBorderColor="black"
+type="text"
+sx={{"border": "0px", "fontWeight": "semibold", "fontSize": "13px", "type": ""}}/></HStack></Container>
+<Container sx={{"borderBottom": "0.3px solid green", "width": "300px", "height": "43px"}}><HStack><LockIcon sx={{"color": "green", "fontSize": "12px"}}/>
+<Input placeholder="PassWord"
+focusBorderColor="black"
+type="text"
+sx={{"border": "0px", "fontWeight": "semibold", "fontSize": "13px", "type": "password"}}/></HStack></Container>
+<Button sx={{"float": "right"}}><Text sx={{"fontSize": "12px", "color": "blue", "textAlign": "end"}}>{`비밀번호를 잊어버리셨나요?`}</Text></Button>
+<Container sx={{"height": "55px"}}/>
+<HStack><Button colorScheme="blue"><Text sx={{"color": "black", "fontSize": "13px", "weight": "bolder"}}>{`회원가입`}</Text></Button>
+<Container sx={{"width": "130px"}}/>
+<Button colorScheme="blue"><Text sx={{"color": "black", "fontSize": "13px", "weight": "bolder", "textAlign": "end"}}>{`로그인`}</Text></Button></HStack></VStack></Container>
+<NextHead><title>{`Pynecone App`}</title>
+<meta name="description"
+content="A Pynecone app."/>
+<meta property="og:image"
+content="favicon.ico"/></NextHead></Container>
+)
+}
---
+++ ProjectSNS/ProjectSNS.py
@@ -0,0 +1,133 @@
+import pynecone as pc
+
+def get_input_field(icon: str, placeholder: str, _type: str):
+    return pc.container(
+        pc.hstack(
+            pc.icon(
+                tag=icon,
+                color='green',
+                fontSize='12px',
+            ),
+            pc.input(
+                placeholder=placeholder,
+                border='0px',
+                focus_border_color='black',
+                fontWeight='semibold',
+                fontSize='13px',
+                type=_type,
+            ),
+        ),
+        borderBottom='0.3px solid green',
+        width='300px',
+        height='43px',
+    )
+
+
+def index():
+    login_container = pc.container(
+        pc.vstack(
+            pc.container(height= '75px'),
+            pc.container(
+                pc.text(
+                    'ONESTARGRAM',
+                    style= {
+                        "fontSize": "25px",
+                        "fontWeight": "bolder",
+                        "letterSpacing": "5px",
+                        "fontFamily": "Georgia, Serif",
+                        "background": "-webkit-linear-gradient(-45deg, #fa0000, #f0b46c)",
+                        "-webkit-background-clip": "text",
+                        "color": "transparent",
+                    },
+                    center_content =True,
+                ),
+            ),
+        ),
+        pc.vstack(
+            pc.container(height= '10px'),
+            pc.container(
+                pc.text(
+                    'Please log in to access ONESTARGRAM.',
+                    style= {
+                        "fontSize": "15px",
+                        "letterSpacing": "2px",
+                        "color": 'black'
+                    },
+                    center_content =True,
+                ),
+            ),
+            pc.container(
+                pc.image(
+                    src="onestar2.ico",
+                    alt="노란별",
+                    style={"width": "100px", "height": "120px"},
+                ),
+                center_content=True,
+            ),
+            get_input_field('EmailIcon','Email',''),
+            get_input_field('LockIcon','PassWord','password'),
+            pc.button(
+                pc.text(
+                    '비밀번호를 잊어버리셨나요?',
+                    style = {
+                        'fontSize':'12px',
+                        'color':'blue',
+                        'textAlign':'end',
+                    },
+                ),
+                style = {
+                    'float':'right',
+                },
+            ),
+            pc.container(height='55px'),
+            pc.hstack(
+                pc.button(
+                    pc.text(
+                        '회원가입',
+                        style = {
+                            'color':'black',
+                            'fontSize':'13px',
+                            'weight':'bolder',
+                        },
+                    ),
+                    color_scheme='blue',
+                ),
+                pc.container(width='130px'),
+                pc.button(
+                    pc.text(
+                        '로그인',
+                        style={
+                            'color':'black',
+                            'fontSize':'13px',
+                            'weight':'bolder',
+                            'textAlign':'end',
+                        },
+                    ),
+                    color_scheme='blue',
+                )
+            ),
+        ),
+        
+        width ='500px',
+        height='75vh',
+        center_content=True,
+        bg = '#ffffff',
+        borderRadius='15px',
+        boxShadow='-11px 11px 50px #9ecadb'
+    )
+
+    _main = pc.container(
+        login_container,
+        center_content=True,
+        justifyContent='center',
+        maxWidth='auto',
+        #width='80vh' 가로 길이
+        height='100vh', #세로 길이
+        bg='#000000', #배경 색상
+    )
+    return _main
+
+
+app = pc.App()
+app.add_page(index)
+app.compile()
